be508b9d8659a52d609dff20004558a17b850a7a
ThomasFTCC <157392618+ThomasFTCC@users.noreply.github.com>
2024-01-22 16:16:25 +0000

    Completed M1T2

---
+++ M1T2_Shull.cpp
@@ -0,0 +1,24 @@
+// CSC 134
+// M1T1
+// Thomas Shull
+// 1/22/24
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Introduce Myself
+    cout << "Hello World!" << "\n";
+    cout << "My name is Thomas Shull." << "\n";
+    cout << "I am a student at FTCC and I am going for an engineering degree." << "\n";
+    cout << "I enjoy studying space related topics and learning about ways we will put humans in space." << "\n";
+
+    // Ask a question
+    string name;
+    cout << "What's your name? ";
+    cin >> name;
+
+    cout << "Nice to meet you, "<< name << endl;
+
+    return 0;
+}
