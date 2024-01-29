3991542cd44bda738bcfbc0522f237dd2c72e4d6
ThomasFTCC <157392618+ThomasFTCC@users.noreply.github.com>
2024-01-29 16:33:42 +0000

    M2T1 - Finished Reciept

--- M2/M2T1_Shull.cpp
+++ M2/M2T1_Shull.cpp
@@ -5,25 +5,33 @@
 
 #include <iostream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
     cout << "M2T1" << endl;
+    cout << "Thank you for dining with us" << endl;
+    cout << "--------------------" << endl;
     // Set up Vars
     string meal = "Value Meal";
     // all in dollars except the precent
-    double meal_price = 5.99;
+    double meal_price = 5.99; //could add input rather than static
     double tax_precent = 0.08;
     double tax_amount = 0;
     double total = 0;
 
     // Do Calculations
+    tax_amount = meal_price * tax_precent;
+    total = meal_price + tax_amount;
 
     // Print the receipt
-    // TODO , and fix decimals
+    // print this once to set the decimals to 2
+    cout << fixed << setprecision(2);
+
     cout << meal << ":" << "\t$" << meal_price << endl;
     cout << "Tax:" << "\t\t$" << tax_amount << endl;
+    cout << "--------------------" << endl;
     cout << "Total:" << "\t\t$" << total << endl;
     return 0;
 }
