603ca213ca5bde74d132c3c5ed2b754050cfd861
ThomasFTCC <157392618+ThomasFTCC@users.noreply.github.com>
2024-02-19 16:41:52 +0000

    Worked on personal projecg

--- ChooseownAdvendureSpace.cpp
+++ ChooseownAdvendureSpace.cpp
@@ -1,49 +1,50 @@
-// compiler directives
-// (required for the program to run)
 #include <iostream>
 using namespace std;
 
 // declare the variable we need
 int choice; 
 string cargo; //turn into something that can hold multiple stuff, like a array or something
+string shipUsed;
 
 //Functions
-void shipyardEarth;
+void start();
+void shipyardEarth();
 
-// beginning of the main() method
 int main() {
-  
-  // this program will ask a question and respond to it.
-  // You should run it, and test it by typing in different values.
-  // Example test values: 1, 2, 3, banana (try all of them)
-
-  
-
   // ask the question
-  cout << "You are on Earth, docked at a spaceport. You could go into orbtit(1), go to the shipyard(2), or go to the market(3)" << endl;
-  cout << "Type 1 or 2: "; 
-  cin >> choice;
-
-  // using if, make a decision based on the user's choice
-
-  if (1 == choice) {
-  	cout << "You are in orbit." << endl;
-  }
-  else if (2 == choice) {
-  	cout << "You are in the shipyard" << endl;
-  }
-  else if(3 == choice){
-    cout << "You are in the market" << endl;
-  }
-  else {
-  	cout << "I'm sorry, that is not a valid choice." << endl;
-  }
+  start();
 
   // finish up
-  cout << "Thanks for playing!" << endl; // this runs no matter what they choose
-  return 0; // exit without error
+  cout << "Thanks for playing!" << endl;
+  return 0;
 
-} // end of the main() method
+}
+
+void start(){
+    cout << "Welcome to the Earth-Moon system!" << endl;
+    cout << "To move around the world type the number next to the option, try it out with the start button!" << endl;
+    cout << "Start(1)" << endl ;
+    cin >> choice;
+
+    if (1 == choice)}{
+        cout << "Where would you like to start?" << endl;
+        cout << "Earth Starport(1)" << endl;
+        cout << "Earth Space Station(2)" << endl;
+        cout << "Moon Starport(3)" << endl;
+        cout << "Moon Space Station(4)" << endl;
+
+        cin >> choice;
+        if(1==choice){
+            starportEarth();
+        }else if(2==choice){
+            //spacestationEarth
+        }else if(3==choice){
+            //starportMoon
+        }else if(4==choice){
+            //spacestationMoon
+        }else
+    }
+}
 
 void starportEarth(){
     cout << "You are docked at Earth's starport" << endl;
